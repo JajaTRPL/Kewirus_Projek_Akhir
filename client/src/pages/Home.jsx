073d79e3cae949d901f51168a7735063fa3d3402
Arif Rizky <muhammadarifrizkypratama2004@mail.ugm.ac.id>
2025-11-20 20:08:19 +0700

    new

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useSnapshot } from 'valtio';
 import state from '../store';
 import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
 import { CustomButton } from '../components';
+import logo from "../assets/logo-eazy_mockup.png"
 
 const Home = () => {
   const snap = useSnapshot(state);
@@ -17,7 +18,7 @@ const Home = () => {
         >
           <motion.header {...slideAnimation('down')}>
             <img
-              src="./threejs.png"
+              src={logo}
               alt="logo"
               className="w-8 h-8 object-contain"
             />
@@ -29,7 +30,7 @@ const Home = () => {
           >
             <motion.div {...headTextAnimation}>
               <h1 className="head-text">
-                LET'S <br className="xl:block hidden" /> DO IT.
+                Eazy <br className="xl:block hidden" /> Mock Up.
               </h1>
             </motion.div>
             <motion.div
@@ -37,13 +38,13 @@ const Home = () => {
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
-                Create you unique and exclusive shirt with our brand-new 3D customization tool.{' '}
-                <strong>Unleash your imagination</strong> and define your own style.
+                Ciptakan kaos unik dan eksklusif dengan alat kostumisasi 3D kami.{" "}
+                <strong>Bebaskan imajinasimu</strong> dan wujudkan gaya khasmu dengan Eazy Mock Up.
               </p>
 
               <CustomButton
                 type="filled"
-                title="Customize It"
+                title="Kustomisasi"
                 handleClick={() => (state.intro = false)}
                 customStyles="w-fit px-4 py-2.5 font-bold text-sm"
               />
